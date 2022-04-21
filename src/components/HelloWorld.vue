3f6e3f49bbea25d9fae622e3d70c925685e5ee80
Aleksander Slanina <ale.slanina@gmail.com>
2022-04-21 23:12:24 +0500

    lab fix

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,8 +15,10 @@
         <v-card>
           <v-card-title class="text-h5 grey lighten-2">
            {{dialog_message}}
-            <v-icon>
-             mdi-cross
+            <v-icon
+            :color="icon_color"
+            class="ml-3 mdi-spin">
+              {{dialog_icon}}
             </v-icon>
           </v-card-title>
         </v-card>
@@ -28,12 +30,19 @@
 
         <p class="subheading font-weight-regular">
           <v-btn
+          class="mr-3 mt-3"
+          elevation="3"
+          color="red lighten-2"
           @click="handle_button()">
-            Ошибиться
+            Создать ошибку
           </v-btn>
+
           <v-btn
+          color="green lighten-2"
+          class="ml-3 mt-3"
+          elevation="3"
           @click="handle_button2()">
-            Серьезно ошибиться
+            Создать сообщение
           </v-btn>
         </p>
       </v-col>
@@ -42,24 +51,34 @@
 </template>
 
 <script>
+import * as Sentry from "@sentry/vue";
+Sentry.setTag("custom_tag", "VueJS");
+Sentry.setTag("another_tag", "Vue > React");
+
   export default {
     name: 'HelloWorld',
 
     data: () => ({
       dialog: false,
       dialog_message: null,
+      dialog_icon: null,
+      icon_color: null,
     }),
     methods: {
       handle_button(){
         this.dialog = true
-        this.dialog_message = 'Вы поступили в СурГУ'
-        throw new ReferenceError('Sentry error')
+        this.dialog_message = 'Ошибка создана'
+        this.dialog_icon = 'mdi-close'
+        this.icon_color = 'red'
+        throw new Error('Sentry error')
 
       },
       handle_button2(){
         this.dialog = true
-        this.dialog_message = 'Вы получили диплом СурГУ'
-        throw new Error('Another error')
+        this.dialog_message = 'Сообщение создано'
+        this.dialog_icon = 'mdi-check'
+        this.icon_color = 'green'
+        Sentry.captureMessage("Sentry message");
       }
     }
   }
